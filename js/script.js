e260969dfeff6f3194f106d23fa1a574bc9c8111
roberto-palanga <r.palanga94@gmail.com>
2024-02-21 16:28:38 +0100

    Milestone completata

--- js/script.js
+++ js/script.js
@@ -6,7 +6,22 @@ createApp({
     data() {
 
         return{
-            parola:"cassone"
+            
+            mails: [],
         }
+    },
+    
+    mounted(){
+        for ( let i = 0; i < 10; i++){
+            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((result) => {
+    
+                console.log(result)
+            
+                this.mails.push(result.data.response)
+    
+            });
+
+        }
+
     }
 }).mount(`#app`);
